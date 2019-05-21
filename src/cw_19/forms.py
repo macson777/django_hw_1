9196e1d2138f224479cd79459597c6aa01daf369
macson777 <macson7777@gmail.com>
2019-05-21 21:39:29 +0300

    add forms

---
+++ src/cw_19/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class PostForm(forms.Form):
+   name = forms.CharField(max_length=20)
+   lastname = forms.CharField(max_length=20)
+   age = forms.IntegerField(min_value=0, max_value=99)
+   comment = forms.CharField(max_length=300)
--- src/cw_19/views.py
+++ src/cw_19/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
-from django.template import loader
 from django.http import HttpResponse
+from cw_19.forms import PostForm
 
 # Create your views here.
 
@@ -9,11 +8,17 @@ from django.http import HttpResponse
 def show_form(request):
     if request.method == 'GET':
         return render(request, 'cw_19_task_1.html')
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        lastname = request.POST.get('lastname')
-        age = request.POST.get('age')
-        comment = request.POST.get('comment')
-        print(f'{name} {lastname} {age} {comment}')
-        return render(request, 'cw_19_task_1.html')
+    elif request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            data = form.data
+            firstname = data.get('name')
+            lastname = data.get('lastname')
+            age = data.get('age')
+            comment = data.get('comment')
+            print(f'{firstname}|{lastname}|{age}|{comment}')
+            return render(request, 'cw_19_task_1.html')
+        else:
+            errors = form.errors
+            return HttpResponse(f'{errors}')
     return HttpResponse('Wrong request method')
