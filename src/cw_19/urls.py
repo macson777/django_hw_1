3fb5b8f325e30cd643df0e8735559349e9693436
macson777 <macson7777@gmail.com>
2019-05-21 21:11:35 +0300

    CW 19 add URL  name

--- src/cw_19/urls.py
+++ src/cw_19/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from cw_19.views import show_form
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('show_form/', show_form),
+    path('show_form/', show_form, name='show_form'),
 ]
